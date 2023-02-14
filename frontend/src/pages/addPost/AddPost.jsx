8194baa455364af726881921337a7c8847a4db64
Pawan Singh <almightycoder4@gmail.com>
2023-02-14 10:39:38 +0530

    add post

--- frontend/src/pages/addPost/AddPost.jsx
+++ frontend/src/pages/addPost/AddPost.jsx
@@ -1,10 +1,52 @@
 import React from "react";
+import { useState } from "react";
 import "../addPost/addpost.css";
 export default function AddPost() {
+  const [add, setadd] = useState(true);
+  const [data, setdata] = useState([]);
   return (
     <div>
       <div>
-        <h3>Add Post + </h3>
+        {add ? (
+          <div>
+            <div id="addbox">
+              <div id="addhead">
+                <h3>Think,Create & Post</h3>
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  placeholder="Enter your Blog Tittle"
+                />
+              </div>
+              <div id="addbody">
+                <textarea
+                  name="content"
+                  id="content"
+                  cols="30"
+                  rows="10"
+                  placeholder="Type your content"
+                ></textarea>
+                <div id="addimg">
+                  <input
+                    type="file"
+                    name="contentimg"
+                    onChange={(event) => {
+                      setdata({
+                        ...data,
+                        [event.target.name]: URL.createObjectURL(
+                          event.target.files[0]
+                        ),
+                      });
+                    }}
+                  ></input>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <h3>Add Post + </h3>
+        )}
       </div>
     </div>
   );
